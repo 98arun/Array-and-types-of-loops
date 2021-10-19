15db56d288ea0df09d67b3bd79505b0df7453c8f
98arun <82587103+98arun@users.noreply.github.com>
2021-10-19 17:21:33 +0530

    Completed the leetcode assignment 3rd

--- assets/js/buy&sell_stock.js
+++ assets/js/buy&sell_stock.js
@@ -1,13 +1,12 @@
 /**
  * Create a function name called as maxProfit
  * In this function it takes a parameter as array like [7,1,2,6,5,4].
+ * let assign a variable as min which take a minimum number
+ * now assign profit as 0
  * Now we have to initialize a loop inside the function which iterates the array (6 times).
- * After iteration it pick one value from array then sort the array suppose it picks 2 and sort them as [4,5,6].
- * Now we have a picked value and a sorted array. now it pick the bigger value on the 
-   sorted array suppose in this array [4,5,6] it pick 6 and subtract it from first picked value as 6-2=4
-    and pushed it on a new array.[4,',',',']
- * After the loop was completed 6 times we have a new array like [4,1,0,2,5]. we have to sort it again as [0,1,2,4,5]
-   and pick the bigger value as 5 from the new array so that value is our maxProfit.
+ * If the value of first parameter is less then minimum value then min value equals to parameter of first value.
+ * Now in second condition if the profit is less then p[i]-min then profit equals to profit = p[i]-min
+ * At last return the profit so we get our maxProfit.
  */
 
 const maxProfit = (p) => {
